fbed50c11825ce7c0d3d4256d6edaf609d9d9fba
Jean Moreira <jeancm110@gmail.com>
2024-10-23 20:25:43 -0300

    Cartões de expansão

---
+++ script.js
@@ -0,0 +1,14 @@
+const panels = document.querySelectorAll('.panel');
+
+panels.forEach((panel) => {
+  panel.addEventListener('click', () => {
+    removeEventClasses();
+    panel.classList.add('active');
+  });
+});
+
+function removeEventClasses() {
+  panels.forEach((panel) => {
+    panel.classList.remove('active');
+  });
+}
